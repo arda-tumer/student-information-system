5b9ba06640eb9143651df0bd8de13db7fc56da47
Arda Tümer <arda.tumer@stu.fbu.edu.tr>
2026-01-09 16:17:59 +0300

    Update logger.cpp
    - Change one line that doesn't follow the clang formatting rules

--- src/logger.cpp
+++ src/logger.cpp
@@ -6,9 +6,12 @@ void logMessage(const std::string &level, const std::string &message)
 {
     std::ofstream logFile("/app/logs/app.log", std::ios::app);
     if (!logFile)
+    {
         return;
-
+    }
+    
     std::time_t now = std::time(nullptr);
     logFile << "[" << std::ctime(&now) << "] "
-            << "[" << level << "] " << message << "\n";
+            << "[" << level << "] " 
+            << message << "\n";
 }
